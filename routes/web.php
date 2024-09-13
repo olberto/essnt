3916f96933a0d3f7bc3dcdf001bee510044df8c6
Olberto Morais <olberto1@hotmail.com>
2024-09-13 09:22:52 -0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\ClienteController;
+
+Route::resource('clientes', ClienteController::class);
+
+Route::get('clientes/{cliente}', [ClienteController::class, 'show'])->name('clientes.show');
